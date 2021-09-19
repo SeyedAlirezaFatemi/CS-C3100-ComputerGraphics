4ec744c7daec045f35c1a40a0112cef3bb26ea20
Alireza <alirezafa@rocketmail.com>
2021-09-19 22:38:35 +0300

    :recycle: Reformat.

--- assignment1-dist/src/base/App.hpp
+++ assignment1-dist/src/base/App.hpp
@@ -6,72 +6,72 @@
 #include <string>
 #include <vector>
 
-
-namespace FW {
-
-struct Vertex
+namespace FW
 {
-	Vec3f position;
-	Vec3f normal;
-};
 
-struct glGeneratedIndices
-{
-	GLuint static_vao, dynamic_vao;
-	GLuint shader_program;
-	GLuint static_vertex_buffer, dynamic_vertex_buffer;
-	GLuint model_to_world_uniform, world_to_clip_uniform, shading_toggle_uniform;
-};
-
-class App : public Window::Listener
-{
-private:
-	enum CurrentModel
+	struct Vertex
 	{
-		MODEL_EXAMPLE,
-		MODEL_USER_GENERATED,
-		MODEL_FROM_INDEXED_DATA,
-		MODEL_FROM_FILE
+		Vec3f position;
+		Vec3f normal;
 	};
 
-public:
-						App();		// constructor
-	virtual				~App() {}	// destructor
+	struct glGeneratedIndices
+	{
+		GLuint static_vao, dynamic_vao;
+		GLuint shader_program;
+		GLuint static_vertex_buffer, dynamic_vertex_buffer;
+		GLuint model_to_world_uniform, world_to_clip_uniform, shading_toggle_uniform;
+	};
 
-	virtual bool		handleEvent(const Window::Event& ev);
+	class App : public Window::Listener
+	{
+	private:
+		enum CurrentModel
+		{
+			MODEL_EXAMPLE,
+			MODEL_USER_GENERATED,
+			MODEL_FROM_INDEXED_DATA,
+			MODEL_FROM_FILE
+		};
 
-private:
-						App(const App&);		// forbid copy
-	App&				operator=(const App&);	// forbid assignment
+	public:
+		App();			  // constructor
+		virtual ~App() {} // destructor
 
-	void				initRendering();
-	void				render();
-	std::vector<Vertex>	loadObjFileModel(std::string filename);
+		virtual bool handleEvent(const Window::Event &ev);
 
-	void				streamGeometry(const std::vector<Vertex>& vertices);
+	private:
+		App(const App &);			 // forbid copy
+		App &operator=(const App &); // forbid assignment
 
-	Window				window_;
-	CommonControls		common_ctrl_;
+		void initRendering();
+		void render();
+		std::vector<Vertex> loadObjFileModel(std::string filename);
 
-	CurrentModel		current_model_;
-	bool				model_changed_;
-	bool				shading_toggle_;
-	bool				shading_mode_changed_;
+		void streamGeometry(const std::vector<Vertex> &vertices);
 
-	glGeneratedIndices	gl_;
+		Window window_;
+		CommonControls common_ctrl_;
 
-	size_t				vertex_count_;
+		CurrentModel current_model_;
+		bool model_changed_;
+		bool shading_toggle_;
+		bool shading_mode_changed_;
 
-	float				camera_rotation_angle_;
+		glGeneratedIndices gl_;
 
-	// YOUR CODE HERE (R1)
-	// Add a class member to store the current translation.
+		size_t vertex_count_;
 
-	// EXTRA:
-	// For animation extra credit you can use the framework's Timer class.
-	// The .start() and .unstart() methods start and stop the timer; when it's
-	// running, .end() gives you seconds passed after last call to .end().
-	// Timer timer_;
-};
+		float camera_rotation_angle_;
+
+		// YOUR CODE HERE (R1)
+		// Add a class member to store the current translation.
+
+		// EXTRA:
+		// For animation extra credit you can use the framework's Timer class.
+		// The .start() and .unstart() methods start and stop the timer; when it's
+		// running, .end() gives you seconds passed after last call to .end().
+		// Timer timer_;
+	};
 
 }
