c043b88aac76dc052fced478332b0e37a4d9736e
Alireza <alirezafa@rocketmail.com>
2021-10-10 17:41:43 +0300

    :recycle: Reformat.

--- assignment2-dist/src/basis/cameraPath.cpp
+++ assignment2-dist/src/basis/cameraPath.cpp
@@ -1,52 +1,45 @@
 #include "cameraPath.h"
-#include <iostream>
 #include <array>
+#include <iostream>
 
 using namespace FW;
 
 
-Mat4f FW::cameraPath::GetOrientation(float t)
-{
-	if (orientationMode)
-	{
-		int count = orientationPoints.size();
-		int start = int(t * count);
+Mat4f FW::cameraPath::GetOrientation(float t) {
+    if (orientationMode) {
+        int count = orientationPoints.size();
+        int start = int(t * count);
 
-		std::array<Vec4f, 4> controlPoints = orientationPoints[start % orientationPoints.size()];
+        std::array<Vec4f, 4> controlPoints = orientationPoints[start % orientationPoints.size()];
 
-		// YOUR CODE HERE (extra)
-		// Use the De Casteljau construction with spherical interpolation (slerp) to interpolate between the orientation control point
-		// quaternions in the points array, and convert the interpolated quaternion to an orientation matrix.
+        // YOUR CODE HERE (extra)
+        // Use the De Casteljau construction with spherical interpolation (slerp) to interpolate between the orientation control point
+        // quaternions in the points array, and convert the interpolated quaternion to an orientation matrix.
 
-		return Mat4f();
-	}
-	else
-	{
-		Mat4f orientation;
-		int i = int(t * positionPath.size());
+        return Mat4f();
+    } else {
+        Mat4f orientation;
+        int i = int(t * positionPath.size());
 
-		orientation.setCol(0, -Vec4f(positionPath[i].B, 0));
-		orientation.setCol(1, -Vec4f(positionPath[i].N, 0));
-		orientation.setCol(2, -Vec4f(positionPath[i].T, .0f));
-		return orientation.transposed();
-	}
+        orientation.setCol(0, -Vec4f(positionPath[i].B, 0));
+        orientation.setCol(1, -Vec4f(positionPath[i].N, 0));
+        orientation.setCol(2, -Vec4f(positionPath[i].T, .0f));
+        return orientation.transposed();
+    }
 }
 
-Mat4f FW::cameraPath::GetTranslation(float t)
-{
-	int i = int(t * positionPath.size());
+Mat4f FW::cameraPath::GetTranslation(float t) {
+    int i = int(t * positionPath.size());
 
-	Vec3f pos = positionPath[i].V;
-	return Mat4f::translate(-pos);
+    Vec3f pos = positionPath[i].V;
+    return Mat4f::translate(-pos);
 }
 
-Mat4f FW::cameraPath::GetWorldToCam(float t)
-{
-	return GetOrientation(t) * GetTranslation(t);
+Mat4f FW::cameraPath::GetWorldToCam(float t) {
+    return GetOrientation(t) * GetTranslation(t);
 }
 
-void FW::cameraPath::Draw(float t, GLContext* gl, Mat4f projection)
-{
-	mesh->draw(gl, GetWorldToCam(t), projection);
-	glUseProgram(0);
+void FW::cameraPath::Draw(float t, GLContext *gl, Mat4f projection) {
+    mesh->draw(gl, GetWorldToCam(t), projection);
+    glUseProgram(0);
 }
