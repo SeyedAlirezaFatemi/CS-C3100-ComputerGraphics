3a44deff752d4669c2b28b7bf590a8a0b1b33cf0
Alireza <alirezafa@rocketmail.com>
2021-12-12 16:58:23 +0200

    :sparkles: Implemented jittered sampling. R9 done.

--- assignment5-dist/src/four/Sampler.cpp
+++ assignment5-dist/src/four/Sampler.cpp
@@ -38,7 +38,6 @@ Vec2f RegularSampler::getSamplePosition(int n) {
     // YOUR CODE HERE (R9)
     // Return a sample through the center of the Nth subpixel.
     // The starter code only supports one sample per pixel.
-    this->m_dim;
     int col = n % this->m_dim;
     int row = static_cast<int>(n / this->m_dim);
     return Vec2f(1.0f / (2 * this->m_dim) + static_cast<float>(col) * 1.0f / (this->m_dim), 1.0f / (2 * this->m_dim) + static_cast<float>(row) * 1.0f / (this->m_dim));
@@ -54,5 +53,8 @@ JitteredSampler::JitteredSampler(int nSamples) : Sampler(nSamples) {
 Vec2f JitteredSampler::getSamplePosition(int n) {
     // YOUR CODE HERE (R9)
     // Return a randomly generated sample through Nth subpixel.
-    return Vec2f(0, 0);
+    const auto range = 1.0f / (2 * this->m_dim);
+    int col = n % this->m_dim;
+    int row = static_cast<int>(n / this->m_dim);
+    return Vec2f(1.0f / (2 * this->m_dim) + static_cast<float>(col) * 1.0f / (this->m_dim), 1.0f / (2 * this->m_dim) + static_cast<float>(row) * 1.0f / (this->m_dim)) + this->m_generator.getVec2f(-range, range);
 }
